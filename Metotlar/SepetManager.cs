9d056ea7291cec7d4d0f4f3e56b4fc77165ac576
Sevdenur <nur.bayram@bil.omu.edu.tr>
2021-01-21 16:22:52 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete Eklendi:" + urun.Adi);
+        }
+
+        
+        //ekstra bir parametre girilmek istendiği zaman aşağıda eklenin yapacağı iş de değişir.
+        //Parametre değistiğinde metodun geçtiği her yer değişmek zorunda kalır.
+        //Bu yuzden bu metodu bu şekilde yazmak tavsiye edilmez...
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stok)
+        {
+            Console.WriteLine("Tebrikler Sepete Eklendi : " + urunAdi);
+        }
+    }
+}
