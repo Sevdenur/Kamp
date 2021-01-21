871d8291209afd370df667769de3f58f73edeb93
Sevdenur <nur.bayram@bil.omu.edu.tr>
2021-01-21 16:40:36 +0300

    edittest

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -15,7 +15,7 @@ namespace DegerVeReferansTipler
             Console.WriteLine(sayi1);
 
             int[] sayilar1 = new int[] { 10, 20, 30 };
-            int[] sayilar2 = new int[] { 100, 200, 300 };
+            int[] sayilar2 = new int[] { 100, 200, 400 };
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
             Console.WriteLine(sayilar1[0]);
